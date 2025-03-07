bc140b1a17bc5f4bd55f77c50974459d38c85b5e
Job Hernandez <lara2993@proton.me>
2025-03-06 18:46:36 -0800

    refactor code

--- AutoDiff.hs
+++ AutoDiff.hs
@@ -13,11 +13,11 @@ square :: Num a => a -> a
 square a = a*a
 
 instance Num a => Num (Dual a) where
-  (Dual p1 d1) + (Dual p2 d2) = dualNumAdd (Dual p1 d1) (Dual p2 d2)
-  (Dual p1 d1) - (Dual p2 d2) = dualNumSub (Dual p1 d1) (Dual p2 d2)
-  (Dual p1 d1) * (Dual p2 d2) = dualNumMul (Dual p1 d1) (Dual p2 d2)
-  signum (Dual p1 d1) = dualNumSignum (Dual p1 d1)
-  abs (Dual p1 d1) = dualNumAbs (Dual p1 d1)
+  p1 + p2 = dualNumAdd p1 p2
+  p1 - p2 = dualNumSub p1 p2
+  p1 * p2 = dualNumMul p1 p2
+  signum p1 = dualNumSignum p1
+  abs p1 = dualNumAbs p1
   fromInteger n = dualNumFromInteger n
   
 dualNumAdd :: Num a => (Dual a) -> (Dual a) -> (Dual a)
@@ -44,9 +44,9 @@ dualNumFromInteger a =
   Dual (fromInteger a) 0
 
 instance (Fractional a) => Fractional (Dual a) where
-  (Dual p1 d1) / (Dual p2 d2) = dualFracDiv (Dual p1 d1) (Dual p2 d2)
+  p1 / p2 = dualFracDiv p1 p2
   fromRational n = dualFracRational n
-  recip (Dual p1 d1) = dualRecip (Dual p1 d1)
+  recip p1 = dualRecip p1
 
 dualFracDiv :: Fractional a => (Dual a) -> (Dual a) -> (Dual a)
 dualFracDiv (Dual p1 d1) (Dual p2 d2) =
@@ -88,12 +88,12 @@ instance Trig Float where
   pi' = pi
   
 instance (Floating a, Num a, Trig a) => (Trig (Dual a)) where
-  sin' (Dual p1 p2) = dualSin (Dual p1 p2)
-  cos' (Dual p1 p2) = dualCos (Dual p1 p2)
-  tan' (Dual p1 p2) = dualTan (Dual p1 p2)
-  asin' (Dual p1 p2) = dualAsin (Dual p1 p2)
-  acos' (Dual p1 p2) = dualAcos (Dual p1 p2)
-  atan' (Dual p1 p2) = dualAtan (Dual p1 p2)
+  sin' p1 = dualSin p1
+  cos' p1 = dualCos p1
+  tan' p1 = dualTan p1
+  asin' p1 = dualAsin p1
+  acos' p1 = dualAcos p1
+  atan' p1 = dualAtan p1
   pi' = (Dual pi 0)
 
 dualSin :: Floating a => Dual a -> Dual a
